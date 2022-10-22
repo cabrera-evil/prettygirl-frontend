7436b91bccc8e3adcd93fdce8c56685ae463164e
cabrera-evil <douglascabrera.dev@gmail.com>
2022-10-22 10:21:57 -0600

    Update: Packages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const mongoose = require("mongoose");
-const cloudinary = require("cloudinary").v2;
-require('dotenv').config();
 const userRoutes = require("./routes/category");
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 9001;
@@ -21,12 +20,5 @@ mongoose
     .connect(process.env.MONGODB_CNN)
     .then(() => { console.log("Connected to MongoDB Atlas") })
     .catch((err) => { console.log(err) });
-
-//Cloudinary Connection
-cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET 
-});
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
