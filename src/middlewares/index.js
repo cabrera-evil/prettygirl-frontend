dddea7b96c9e440d19c086199f30e7536e8e99e7
cabrera-evil <douglascabrera.dev@gmail.com>
2022-11-11 10:02:36 -0600

    Update: Routes

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const userRoutes = require("../routes/user");
+//const userRoutes = require("../routes/user");
 
 const config = async (app) => {
     app.use(express.json());
-    app.use('/api', userRoutes);
+    //app.use('/api', userRoutes);
 }
 
 module.exports = {config};
