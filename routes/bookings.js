b6cf4a231ff795f1af480dd257688f2c5008f041
cabrera-evil <douglascabrera.dev@gmail.com>
2022-11-27 17:30:18 -0600

    Update: Booking validations

--- routes/bookings.js
+++ routes/bookings.js
@@ -14,7 +14,7 @@ const {
 } = require("../controllers/bookings");
 
 // Import helpers
-const { bookingExistByID, productExistByID, userExistByID } = require("../helpers/db-validators");
+const { bookingExistByID, userExistByID } = require("../helpers/db-validators");
 
 const router = Router();
 
@@ -39,7 +39,6 @@ router.post(
     [
         validateJWT,
         check("description.products", "products are required").not().isEmpty(),
-        check("description.products").isArray().custom(productExistByID),
         check("user", "User is required").not().isEmpty(),
         check("user").custom(userExistByID),
         check("address", "Address is required").not().isEmpty(),
@@ -55,7 +54,6 @@ router.put(
         validateJWT,
         isAdminRole,
         check("description.products", "products are required").not().isEmpty(),
-        check("description.products").isArray().custom(productExistByID),
         check("id").custom(bookingExistByID),
         check("user", "User is required").not().isEmpty(),
         check("user").custom(userExistByID),
